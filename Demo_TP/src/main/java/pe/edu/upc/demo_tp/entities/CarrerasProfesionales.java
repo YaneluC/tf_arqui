8cf4f55d5e0434220e816cae302636c041b3a002
KiaraZun <89083290+KiaraZun@users.noreply.github.com>
2023-09-05 19:38:23 -0500

    Update CarrerasProfesionales.java

--- Demo_TP/src/main/java/pe/edu/upc/demo_tp/entities/CarrerasProfesionales.java
+++ Demo_TP/src/main/java/pe/edu/upc/demo_tp/entities/CarrerasProfesionales.java
@@ -1,1 +1,60 @@
-jbkjbkbkb
+package pe.edu.upc.aaw.demo_tp.entities;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+@Entity
+@Table(name = "CarrerasProfesionales")
+public class CarrerasProfesionales {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCarreraProfesional;
+    @Column(name = "codCarreraProfesional",length = 10,nullable = false)
+    private String codCarreraProfesional;
+    @Column(name = "nombreCarreraProfesional",length = 100,nullable = false)
+    private String nombreCarreraProfesional;
+    @Column(name = "idUniversidad",length=10,nullable = false)
+    private int idUniversidad;
+
+    public CarrerasProfesionales() {
+    }
+
+    public CarrerasProfesionales(int idCarreraProfesional, String codCarreraProfesional, String nombreCarreraProfesional, String idUniversidad) {
+        this.idCarreraProfesional = idCarreraProfesional;
+        this.codCarreraProfesional = codCarreraProfesional;
+        this.nombreCarreraProfesional = nombreCarreraProfesional;
+        this.idUniversidad = idUniversidad;
+    }
+
+    public int getidCarreraProfesional() {
+        return idCarreraProfesional;
+    }
+
+    public void setidCarreraProfesional(int idCarreraProfesional) {
+        this.idCarreraProfesional = idCarreraProfesional;
+    }
+
+    public String getcodCarreraProfesional() {
+        return codCarreraProfesional;
+    }
+
+    public void setcodCarreraProfesional(String codCarreraProfesional) {
+        this.codCarreraProfesional = codCarreraProfesional;
+    }
+
+    public String getnombreCarreraProfesional() {
+        return nombreCarreraProfesional;
+    }
+
+    public void setnombreCarreraProfesional(String nombreCarreraProfesional) {
+        this.nombreCarreraProfesional = nombreCarreraProfesional;
+    }
+
+    public String getidUniversidad() {
+        return idUniversidad;
+    }
+
+    public void idUniversidad(String idUniversidad) {
+        this.idUniversidad = idUniversidad;
+    }
+}
