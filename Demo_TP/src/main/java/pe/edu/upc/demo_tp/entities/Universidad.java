fd44256ad63cad03de246875edfc742cb23a91f7
susu <89083290+KiaraZun@users.noreply.github.com>
2023-09-06 13:59:29 -0500

    Update Universidad.java

--- Demo_TP/src/main/java/pe/edu/upc/demo_tp/entities/Universidad.java
+++ Demo_TP/src/main/java/pe/edu/upc/demo_tp/entities/Universidad.java
@@ -18,5 +18,27 @@ public class Universidad {
         this.nombreUniversidad = nombreUniversidad;
     }
 
-  
+    public int getIdUniversidad() {
+        return idUniversidad;
+    }
+
+    public void setIdUniversidad(int idUniversidad) {
+        this.idUniversidad = idUniversidad;
+    }
+
+    public String getCodUniversidad() {
+        return codUniversidad;
+    }
+
+    public void setCodUniversidad(String codUniversidad) {
+        this.codUniversidad = codUniversidad;
+    }
+
+    public String nombreUniversidad() {
+        return nombreUniversidad;
+    }
+
+    public void setnombreUniversidad(String nombreUniversidad) {
+        this.nombreUniversidad = nombreUniversidad;
+    }
 }
