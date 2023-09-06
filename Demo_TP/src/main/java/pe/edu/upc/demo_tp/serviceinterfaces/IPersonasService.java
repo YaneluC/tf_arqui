10175784d38680ed8bc59f60f2ea7a188ecf2400
YaneluC <99844598+YaneluC@users.noreply.github.com>
2023-09-05 19:36:48 -0500

    Add files via upload

---
+++ Demo_TP/src/main/java/pe/edu/upc/demo_tp/serviceinterfaces/IPersonasService.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.demo_tp.serviceinterfaces;
+
+import pe.edu.upc.demo_tp.entities.Personas;
+
+import java.util.List;
+
+public interface IPersonasService {
+    public void insert(Personas personas);
+    public List<Personas> list();
+    public void delete(int idPersonas);
+    public Personas listId(int idPersonas);
+}
