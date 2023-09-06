9ad958c1d5d1f3cf2ecfc5543b8b907fd8eceda8
YaneluC <99844598+YaneluC@users.noreply.github.com>
2023-09-05 19:34:49 -0500

    Add files via upload

---
+++ Demo_TP/src/main/java/pe/edu/upc/demo_tp/dtos/PersonasDTO.java
@@ -0,0 +1,78 @@
+package pe.edu.upc.demo_tp.dtos;
+
+import java.time.LocalDate;
+
+public class PersonasDTO {
+    private int idPersona;
+    private String usuario;
+    private String contraseña;
+    private String correo;
+    private String telefono;
+    private LocalDate fechanac;
+    private String nombre;
+    private String apellido;
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDate getFechanac() {
+        return fechanac;
+    }
+
+    public void setFechanac(LocalDate fechanac) {
+        this.fechanac = fechanac;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
