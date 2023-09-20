bece71d4f01907882823a20a2951b07a19213f56
Raul Lopez <81383203+snayderoz@users.noreply.github.com>
2023-09-20 11:15:09 -0500

    Delete tf_arquitectura_web/src directory

--- tf_arquitectura_web/src/main/java/pe/edu/upc/tf_arquitectura_web/repositories/ICriterioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.tf_arquitectura_web.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.tf_arquitectura_web.entities.Criterio;
-
-@Repository
-public interface ICriterioRepository extends JpaRepository<Criterio, Integer> {
-}
--- tf_arquitectura_web/src/main/java/pe/edu/upc/tf_arquitectura_web/serviceinterfaces/ICarrerasProfesionalesService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.tf_arquitectura_web.serviceinterfaces;
-import pe.edu.upc.tf_arquitectura_web.entities.CarrerasProfesionales;
-import java.util.List;
-public interface ICarrerasProfesionalesService {
-    public  void insert(CarrerasProfesionales carrerasProfesionales);
-    public List<CarrerasProfesionales> list();
-    public void  delete(int idCarrerasProfesionales);
-    public CarrerasProfesionales listId(int idCarrerasProfesionales);
-}
