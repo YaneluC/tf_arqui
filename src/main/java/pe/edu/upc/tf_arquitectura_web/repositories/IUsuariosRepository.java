42f9d59f8b6589d50a2fd8e1195baaa858a2806f
YaneluC <99844598+YaneluC@users.noreply.github.com>
2023-09-20 19:27:32 -0500

    Update IUsuariosRepository.java

--- src/main/java/pe/edu/upc/tf_arquitectura_web/repositories/IUsuariosRepository.java
+++ src/main/java/pe/edu/upc/tf_arquitectura_web/repositories/IUsuariosRepository.java
@@ -1,10 +1,15 @@
 package pe.edu.upc.tf_arquitectura_web.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.tf_arquitectura_web.entities.Usuarios;
 
+import java.util.List;
+
 @Repository
 public interface IUsuariosRepository extends JpaRepository<Usuarios, Integer>{
-
+    @Query(value = "select nombre, apellido, fechanac from Usuarios order by fechanac asc",nativeQuery = true)
+    public List<String[]> ordensegunfechanacimiento();
 }
+
