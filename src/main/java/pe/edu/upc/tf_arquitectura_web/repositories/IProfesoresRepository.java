78e864be63c7edcd2a3a5786c1636bdddc8082e6
GustavoFarje <116670648+GustavoFarje@users.noreply.github.com>
2023-09-22 18:25:27 -0500

    Update IProfesoresRepository.java

--- src/main/java/pe/edu/upc/tf_arquitectura_web/repositories/IProfesoresRepository.java
+++ src/main/java/pe/edu/upc/tf_arquitectura_web/repositories/IProfesoresRepository.java
@@ -1,10 +1,18 @@
 package pe.edu.upc.tf_arquitectura_web.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.tf_arquitectura_web.entities.Profesores;
 
+import java.util.List;
+
 @Repository
 public interface IProfesoresRepository extends JpaRepository<Profesores, Integer>{
+@Query(value = "select u.nombre_universidad, count (p.id_profesores)\n" +
+            " from universidad u inner join profesores p\n" +
+            " on u.id = p.id\n" +
+            " group by u.nombre_universidad",nativeQuery = true)
 
+    public List<String[]> cantidadDeProfesoresPorUniversidad();
 }
